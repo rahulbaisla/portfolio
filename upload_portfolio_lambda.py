efb879cf5bc37af26d1cd5883d37b38b5e564888
Rahul Baisla <rahulbaisla@gmail.com>
2018-08-19 01:07:08 +1000

    Added final code for AWS Code Build.This script downloads, unzips and upload the files back to deployment  folder.

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -1,14 +1,28 @@
 import boto3
+from botocore.client import Config
+import io
 import mimetypes
 import zipfile
-import os
 
-s3_client = boto3.client('s3')
+def lambda_handler(event, context):
 
-s3_client.download_file('portfoliobuild.rahulbaisla.com', 'portfoliobuild.zip', 'temp.zip')
+    # Define s3 resources
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-with zipfile.ZipFile('temp.zip') as myzip:
-    for nm in myzip.namelist():
-        s3_client.upload_file(nm, 'portfolio.rahulbaisla.com',nm,ExtraArgs={'ACL': 'public-read','ContentType':mimetypes.guess_type(nm)[0]})
+    # Define Buckets
+    portfolio_bucket = s3.Bucket("portfolio.rahulbaisla.com")
+    build_bucket = s3.Bucket("portfoliobuild.rahulbaisla.com")
 
-os.remove("temp.zip")
+    # Download the artefacts from s3 Bucket to inmemory
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    # Iterate through files in Zip and upload to portforlio bucket
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ACL':'public-read','ContentType':mimetypes.guess_type(name)[0]})
+
+    print('job done !!')
+
+    return 'Hello from Lambda'
