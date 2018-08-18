a2d8649116d532f80d62ea23bd46c4e21e47959b
Rahul Baisla <rahulbaisla@gmail.com>
2018-08-19 01:21:09 +1000

    Added code for AWS SNS subscription. Lambda function will send SNS notification to configured subscriber about status of deployment.

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -8,21 +8,28 @@ def lambda_handler(event, context):
 
     # Define s3 resources
     s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:ap-southeast-2:998481987873:deployPortfolioTopic')
 
-    # Define Buckets
-    portfolio_bucket = s3.Bucket("portfolio.rahulbaisla.com")
-    build_bucket = s3.Bucket("portfoliobuild.rahulbaisla.com")
+    try:
+        # Define Buckets
+        portfolio_bucket = s3.Bucket("portfolio.rahulbaisla.com")
+        build_bucket = s3.Bucket("portfoliobuild.rahulbaisla.com")
 
-    # Download the artefacts from s3 Bucket to inmemory
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        # Download the artefacts from s3 Bucket to inmemory
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    # Iterate through files in Zip and upload to portforlio bucket
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for name in myzip.namelist():
-            obj = myzip.open(name)
-            portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ACL':'public-read','ContentType':mimetypes.guess_type(name)[0]})
+        # Iterate through files in Zip and upload to portforlio bucket
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for name in myzip.namelist():
+                obj = myzip.open(name)
+                portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ACL':'public-read','ContentType':mimetypes.guess_type(name)[0]})
 
-    print('job done !!')
+        print('job done !!')
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed Successfully.")
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed", Message="Portfolio deployment  Failed.")
+        raise
 
     return 'Hello from Lambda'
