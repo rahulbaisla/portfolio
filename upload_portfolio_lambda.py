d3229d2a6ec52949365d756727218083b4daa7f6
Rahul Baisla <rahulbaisla@gmail.com>
2018-08-18 18:46:54 +1000

    Test AWS CodeBuild

---
+++ upload_portfolio_lambda.py
@@ -0,0 +1,14 @@
+import boto3
+import mimetypes
+Import zipfile
+Import os
+
+s3_client = boto3.client('s3')
+
+s3_client.download_file('portfoliobuild.rahulbaisla.com', 'portfoliobuild.zip', 'temp.zip')
+
+with zipfile.ZipFile('temp.zip') as myzip:
+    for nm in myzip.namelist():
+        s3_client.upload_file(nm, 'portfolio.rahulbaisla.com',nm,ExtraArgs={'ACL': 'public-read','ContentType':mimetypes.guess_type(nm)[0]})
+
+os.remove("temp.zip")
