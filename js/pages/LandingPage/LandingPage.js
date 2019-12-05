5fc7cc3ed5d93267c5148a8853c85a879afe0697
Bansal <rahbans@f018984aa49a.ant.amazon.com>
2019-12-05 22:34:44 +1100

    Fixed Failing test

--- js/pages/LandingPage/LandingPage.js
+++ js/pages/LandingPage/LandingPage.js
@@ -4,21 +4,20 @@ import React from 'react';
 class  LandingPage extends React.Component {
   render() {
     return (
-
-      <main className="landing-page">
+    <div className="landing-page">
         <main>
           <div id="intro" className="intro">Hello I'm Rahul Baisla</div>
-          <div id="tagLine" className="tagline">AWS Cloud Engineer | Software Developer | Educator</div>
+          <div id="tagLine" className="tagline">AWS Cloud Engineer | Software Developer| Educator</div>
           <div id="social-icons" className="icons-social">
             <a target="_blank" href="https://github.com/rahulbaisla"><i className="fab fa-github" title="GitHub"></i></a>
             <a target="_blank" href="https://www.linkedin.com/in/rahulbaisla"><i className="fab fa-linkedin" title="LinkedIn"></i></a>
             <a target="_blank" href="https://cloudnamaste.com"><i className="fab fa-mixcloud" title="CloudNamaste"></i></a>
             <a target="_blank" href="RahulBaisla-Resume.pdf"><i className="fas fa-file-alt" title="Download Resume"></i></a>
             <a target="_blank" href="https://www.facebook.com/rahulbaisla"><i className="fab fa-facebook" title="Facebook"></i></a>
-            <a target="_blank" href="https://www.instagram.com/strength.letic"><i className="fab fa-instagram" title="Instagram"></i></a>
+            <a target="_blank" href="https://www.instagram.com/strength.letic"><i className="fab fa-instagram" title="Instagram"></i></a> 
           </div>
         </main>
-      </main>
+    </div>
     );
   };
 };
